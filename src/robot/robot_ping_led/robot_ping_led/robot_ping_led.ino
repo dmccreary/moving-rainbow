df1d8b747a03c7fbd2f5dac75824ddf7d27d62be
dmccreary <dan.mccreary@gmail.com>
2015-01-01 18:24:31 -0600

    added code to balance the motors to keep the robot going in a streight line

--- src/robot/robot_ping_led/robot_ping_led/robot_ping_led.ino
+++ src/robot/robot_ping_led/robot_ping_led/robot_ping_led.ino
@@ -3,20 +3,29 @@
 const int pingPin = 12;
 int dist_in_cm = 120;
 
-// LED for distance feedback
+// This LED strip is used for distance feedback
+// The closer we get to an object in front of us, the further up the blue pixel is on
 #include <Adafruit_NeoPixel.h>
 #define LEDPIN 11 // connect the Data from the strip to this pin on the Arduino
 #define NUMBER_PIEXELS 12 // the number of pixels in your LED strip
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMBER_PIEXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+
 int old_strip_index = 0;
 int new_strip_index = 0;
-// motor pins
-int right_forward = 4;
+int power_turn_level = 255; /* full power on turns */
+
+// adjust these till the robot goes streight to compensate for motor differences
+int power_forward_right = 140; /* half power on turns */
+int power_forward_left = 120; /* half power on turns */
+
+// motor pins.  Note that only pins 2,5,6, 9 and 10 can be used for pwm
+int right_forward = 3;
 int right_reverse = 5;
-int left_forward = 7;
+int left_forward = 9;
 int left_reverse = 6;
-// try this time to make a right turn
-int delay_time_ninty_turn = 800;
+
+// try this time to make a right turn just above 90 degrees
+int delay_time_ninty_turn = 200;
 // if we are under this distance, make a turn
 int cm_for_turn = 10;
 int delay_time_forward = 100;
@@ -28,7 +37,7 @@ void setup() {
   strip.setPixelColor(0, 10, 0, 0);
   strip.show();
   
-  pinMode(pingPin, OUTPUT);
+  pinMode(pingPin, INPUT);
   
   pinMode(right_forward, OUTPUT);
   pinMode(right_reverse, OUTPUT); 
@@ -40,11 +49,15 @@ void setup() {
   digitalWrite(left_forward, LOW);
   digitalWrite(left_reverse, LOW);
   // for debugging
-  Serial.println('Start');
+  // Serial.println('Start');
+  delay(1000);
 }
 
 void loop() {
   
+  delay(100);
+  
+  // get the distance from the ping sensor in CM
   dist_in_cm = get_distance_cm();
   new_strip_index = dist_in_cm / 5;
   
@@ -58,8 +71,8 @@ void loop() {
     // erase the old strip
      for (int i=0; i < NUMBER_PIEXELS; i++)
         strip.setPixelColor(i, 0, 0, 0);
-    // turn on new value
-     strip.setPixelColor(new_strip_index, 0, 0, 20);
+    // turn on new value to a light blue
+     strip.setPixelColor(new_strip_index, 0, 0, 30);
      strip.show();
   };
   
@@ -70,36 +83,33 @@ void loop() {
       move_forward();
     }
   
-  Serial.print(" new=");
-  Serial.print(new_strip_index);
+  // Serial.print(" new=");
+  // Serial.print(new_strip_index);
 
-  delay(100);
-  
 }
 
 void turn_right() {
   Serial.println("turning right");
-  digitalWrite(right_forward, LOW);
-  digitalWrite(right_reverse, HIGH);
-  digitalWrite(left_forward, HIGH);
-  digitalWrite(left_reverse, LOW);
+  analogWrite(right_forward, LOW);
+  analogWrite(right_reverse, power_turn_level);
+  analogWrite(left_forward, power_turn_level);
+  analogWrite(left_reverse, LOW);
   delay(delay_time_ninty_turn);
-  
 }
 
 void move_forward() {
   Serial.println("moving forward");
-  digitalWrite(right_forward, HIGH);
-  digitalWrite(right_reverse, LOW);
-  digitalWrite(left_forward, HIGH);
-  digitalWrite(left_reverse, LOW);
+  analogWrite(right_forward, power_forward_right);
+  analogWrite(right_reverse, LOW);
+  analogWrite(left_forward, power_forward_left);
+  analogWrite(left_reverse, LOW);
   delay(delay_time_forward);
 }
 
 
 int get_distance_cm()
 {
-  long duration, inches, cm;
+  long duration, cm=0;
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
@@ -116,16 +126,6 @@ int get_distance_cm()
   Serial.print(" cm=");
   Serial.println(cm);
   return cm;
-}
-
-long microsecondsToInches(long microseconds)
-{
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
 }
 
 long microsecondsToCentimeters(long microseconds)
