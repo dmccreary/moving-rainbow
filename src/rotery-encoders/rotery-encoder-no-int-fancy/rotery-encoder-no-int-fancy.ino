5baac00a581956e674cbb1ba23497046c98634e4
Dan McCreary <dan@danmccreary.com>
2015-08-07 17:28:13 -0500

    added code for fancy rotery encoder

--- src/rotery-encoders/rotery-encoder-no-int-fancy/rotery-encoder-no-int-fancy.ino
+++ src/rotery-encoders/rotery-encoder-no-int-fancy/rotery-encoder-no-int-fancy.ino
@@ -1,4 +1,5 @@
 /* Rotary encoder read example without any interrupt
+Dan McCreary with help by Mary McCreary
 http://www.circuitsathome.com/mcu/programming/reading-rotary-encoder-on-arduino
 connect the center wire to ground.  Connect the side pins to Analog ports 
 Connect the middle pin of the rotery encoder to ground */
@@ -6,7 +7,7 @@ Connect the middle pin of the rotery encoder to ground */
 #define ENC_B A1 // Hook the other end of the rotery encoder to Analog pin A1
 #define ENC_PORT PINC
 #define MODE_PIN 2 // Put one of the push knob here and the other to ground
-#define NUM_MODES 4 // The number of display pattern modes
+#define NUM_MODES 8 // The number of display pattern modes
 
 #include <Adafruit_NeoPixel.h>
 #define LEDPIN 12 // connect the Data from the strip to this pin on the Arduino
@@ -28,22 +29,25 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 150;    // the debounce time; increase if the output flickers
+long debounceDelay = 200;    // the debounce time; increase if the output flickers
 
 void setup()
 {
   strip.begin(); // initialize the LED strip
-  /* Setup encoder pins as inputs */
+  
+  // Setup encoder pins as inputs
   pinMode(ENC_A, INPUT);
   digitalWrite(ENC_A, HIGH);
   pinMode(ENC_B, INPUT);
   digitalWrite(ENC_B, HIGH);
+  
+  // Have the mode float high using the internal pullup resistor, pull down to ground if pressed
   pinMode(MODE_PIN, INPUT_PULLUP);
   
-  attachInterrupt(0, isr, FALLING);  // run the isr program when the push button goes down
+  attachInterrupt(0, isr, FALLING);  // run the isr function when the push button goes down to LOW
   
-  Serial.begin (9600);
-  Serial.println("Start");
+  //Serial.begin (9600);
+  //Serial.println("Start");
 }
 
 void loop()
@@ -55,16 +59,15 @@ void loop()
   // get the new data
   tmpdata = read_encoder(); // get the new value, -1, 0, or 1
   if (tmpdata) {            // we only change if we have a new value
-    Serial.print("Counter=");
-    Serial.print(counter, DEC);
-    Serial.print("  Display mode1=: ");
-    Serial.print(patternMode, DEC);
+    // Serial.print("Counter=");
+    //Serial.print(counter, DEC);
     
-    Serial.print("  Time=: ");
-    Serial.print(lastDebounceTime, DEC);
+    //Serial.print("  Display mode1=: ");
+    //Serial.print(patternMode, DEC);
     
-    Serial.print("  Display mode2=: ");
-    Serial.println(patternMode2, DEC);
+    //Serial.print("  Time=: ");
+    //Serial.print(lastDebounceTime, DEC);
+    
     counter += tmpdata;  // make the counter go down, or up by one - every "click" is 4 changes
   };
   
@@ -86,31 +89,17 @@ int8_t read_encoder()
   return ( enc_states[( old_AB & 0x0f )]);
 }
 
-// Interrupt Service Routine for the pattern mode button press
+// Interrupt Service Routine (ISR) for the pattern mode button press
+// Note that we have a debounce delay check
 void isr() {
-  // read the state of the push button into a local variable:
-  int reading = digitalRead(MODE_PIN);
-
-  // check to see if you just pressed the button 
-  // (i.e. the input went from LOW to HIGH),  and you've waited 
-  // long enough since the last press to ignore any noise:  
-
-  // If the switch changed, due to noise or pressing:
-  if (reading != LOW) {
-    // reset the debouncing timer
-    lastDebounceTime = millis(); // this is the number of milliseconds since power on
-  } 
-  
+  // only increment the mode once every debounceDelay interval
   if ((millis() - lastDebounceTime) > debounceDelay) {
     // whatever the reading is at, it's been there for longer
     // than the debounce delay, so take it as the actual current state:
     // only increment the pattern mode if the new button state is HIGH
-    patternMode = (patternMode + 1) % NUM_MODES;
-    lastButtonState = LOW;
+    patternMode = (patternMode + 1) % NUM_MODES; // add one to the pattern mode but do a modulo
+    lastDebounceTime = millis(); // reset the timer
   }
-  
-  patternMode2 = (patternMode2 + 1) % NUM_MODES;
-
 }
 
 // draw a pattern, erase at the old and draw at the new one
@@ -119,23 +108,43 @@ void drawPattern(uint8_t oldPiexl, uint8_t counter) {
   strip.setPixelColor(oldPixel, 0, 0, 0);
   strip.setPixelColor((oldPixel+1) % NUMBER_PIXELS, 0, 0, 0);
   strip.setPixelColor((oldPixel+2) % NUMBER_PIXELS, 0, 0, 0);
+  strip.setPixelColor((oldPixel+3) % NUMBER_PIXELS, 0, 0, 0);
+  strip.setPixelColor((oldPixel+4) % NUMBER_PIXELS, 0, 0, 0);
+  strip.setPixelColor((oldPixel+5) % NUMBER_PIXELS, 0, 0, 0);
+  strip.setPixelColor((oldPixel+6) % NUMBER_PIXELS, 0, 0, 0);
   // make the new pixel the counter modulo the number of pixels
   newPixel = counter % NUMBER_PIXELS;
   
   // turn on the new pixel
   if (patternMode == 0) {
-    strip.setPixelColor(newPixel, 255, 0, 0);
-    strip.setPixelColor((newPixel + 1) % NUMBER_PIXELS, 0, 255, 0);
-    strip.setPixelColor((newPixel + 2) % NUMBER_PIXELS, 0, 0, 255);
+    strip.setPixelColor(newPixel, 255, 0, 0);      // red
+    strip.setPixelColor((newPixel + 1) % NUMBER_PIXELS, 110, 70, 0); // orange
+    strip.setPixelColor((newPixel + 2) % NUMBER_PIXELS, 255, 255, 0);    // yellow
+    strip.setPixelColor((newPixel + 3) % NUMBER_PIXELS, 0, 255, 0);
+    strip.setPixelColor((newPixel + 4) % NUMBER_PIXELS, 0, 0, 255);
+    strip.setPixelColor((newPixel + 5) % NUMBER_PIXELS, 255, 0, 255);
+
   }
   if (patternMode == 1) {  // red
     strip.setPixelColor(newPixel, 255, 0, 0);
   }
-  if (patternMode == 2) {  //  green
+  if (patternMode == 2) {  //  orange
+    strip.setPixelColor(newPixel, 110, 70, 0);
+  }
+  if (patternMode == 3) { // yellow
+    strip.setPixelColor(newPixel, 255, 255, 0);
+  }
+  if (patternMode == 4) { // green
     strip.setPixelColor(newPixel, 0, 255, 0);
   }
-    if (patternMode == 3) { // blue
+  if (patternMode == 5) { // blue
     strip.setPixelColor(newPixel, 0, 0, 255);
+  }
+  if (patternMode == 6) { // indigo
+    strip.setPixelColor(newPixel, 255, 0, 255);
+  }
+  if (patternMode == 7) { // violet
+    strip.setPixelColor(newPixel, 25, 0, 25);
   }
   strip.show();
 };
