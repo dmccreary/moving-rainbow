60d06982ee2aa47bf77d18cf287f64fa957cc351
Dan McCreary <dan@danmccreary.com>
2015-08-07 17:39:38 -0500

    Changed delay from 200 to 250 for debounce

--- src/rotery-encoders/rotery-encoder-no-int-fancy/rotery-encoder-no-int-fancy.ino
+++ src/rotery-encoders/rotery-encoder-no-int-fancy/rotery-encoder-no-int-fancy.ino
@@ -29,7 +29,7 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 200;    // the debounce time; increase if the output flickers
+long debounceDelay = 250;    // the debounce time; increase if the output flickers
 
 void setup()
 {
