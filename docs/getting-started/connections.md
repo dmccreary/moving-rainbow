27b20a08446522536a8c363abcd46c2a473a61fd
dmccreary <dan.mccreary@gmail.com>
2022-10-14 16:16:20 -0500

    updated content

--- docs/getting-started/connections.md
+++ docs/getting-started/connections.md
@@ -1,5 +1,9 @@
 # Circuit Connections
 
-Because this 
+Because this guide is focused on teaching computational thinking, we don't spend much time on circuits and wiring diagrams.  We try to keep the circuits and wiring diagrams as simple as possible.
+
+For the basic NeoPixel strip are only three wires you need to conn
+
+
 
 ![](../img/pico-on-breadboard-annotated.png)
---
+++ src/16x16-matrix/02-move.py
@@ -0,0 +1,19 @@
+from neopixel import NeoPixel
+from utime import sleep
+
+NEOPIXEL_PIN = 0
+NUMBER_PIXELS = 256
+
+strip = NeoPixel(machine.Pin(NEOPIXEL_PIN), NUMBER_PIXELS)
+
+# Create a varuabke to store the delay 
+delay = .005
+while True:
+    for i in range(0, NUMBER_PIXELS - 1):
+        # turn index red pixel on for and delay
+        strip[i] = (255,0,0)
+        strip.write()
+        sleep(delay)
+
+        # turn off
+        strip[i] = (0,0,0)
