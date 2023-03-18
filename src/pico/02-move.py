4e478a8d304d89edaaa1ea0b8ed8f3da5e546a26
dmccreary <dan.mccreary@gmail.com>
2023-03-18 11:24:03 -0500

    updated content

--- src/pico/02-move.py
+++ src/pico/02-move.py
@@ -2,12 +2,12 @@ from neopixel import NeoPixel
 from utime import sleep
 
 NEOPIXEL_PIN = 0
-NUMBER_PIXELS = 60
+NUMBER_PIXELS = 12
 
 strip = NeoPixel(machine.Pin(NEOPIXEL_PIN), NUMBER_PIXELS)
 
 # Create a varuabke to store the delay 
-delay = .002
+delay = .02
 while True:
     for i in range(0, NUMBER_PIXELS - 1):
         # turn index red pixel on for and delay
