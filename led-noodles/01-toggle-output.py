9f685068ef52c7ff28b941fbf879c56ef25152c9
Dan Mccreary <Dan.McCreary@gmail.com>
2024-09-23 12:49:53 -0500

    updating content

--- led-noodles/01-toggle-output.py
+++ led-noodles/01-toggle-output.py
@@ -4,15 +4,15 @@ from time import sleep
 # lower left corner of the Pico
 LED_PIN_1 = 15
 led_1 = machine.Pin(LED_PIN_1, machine.Pin.OUT)
-delay = 1.0
+delay = 1.0 3 # one second on and one second off
 
 # Main loop: Repeat the forever...
 while True:
     print('high')
     led_1.high() # turn on the LED
-    sleep(delay) # leave it on for 1/2 second
+    sleep(delay) # leave it on for 1 second
     
     print('low')
     led_1.low()  # Turn off the LED
-    sleep(delay) # leave it off for 1/2 second
+    sleep(delay) # leave it off for 1 second
     
